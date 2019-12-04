a7db8612a69cdcc6232b26912102087f666c2383
Dominik Straub <ds@kadenpartner.ch>
2019-12-04 14:48:19 +0100

    Day 4, Star 1

---
+++ Day4/main.js
@@ -0,0 +1,24 @@
+(() => {
+    const min = 165432;
+    const max = 707912;
+    const length = 6;
+    let count = 0;
+    for (let code = min; code <= max; code++) {
+        const codeString = '' + code;
+        let onlyIncrease = true;
+        let sameDigit = false;
+        for (let digit = 0; digit < length - 1; digit++) {
+            if (codeString[digit] > codeString[digit + 1]) {
+                onlyIncrease = false;
+                break;
+            }
+            if (codeString[digit] === codeString[digit + 1]) {
+                sameDigit = true;
+            }
+        }
+        if (onlyIncrease && sameDigit) {
+            count++;
+        }
+    }
+    console.log(count);
+})();
