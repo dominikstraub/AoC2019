01eed35e1dd34ee13c4ffea4bca4b0e9e56ef796
Dominik Straub <ds@kadenpartner.ch>
2019-12-06 10:57:41 +0100

    Day 6, Star 2

--- Day6/main.js
+++ Day6/main.js
@@ -3,6 +3,7 @@
 
     const response = await fetch('./input.json');
     // const response = await fetch('./test.json');
+    // const response = await fetch('./test2.json');
     const input = (await response.json()).data;
     // console.log(input);
 
@@ -34,6 +35,43 @@
 
     const orbits = countOrbits(uom['COM'], 0);
     console.log(orbits);
+
+    const buildPath = path => {
+        const orbiter = path[0];
+        for (const orbiteeKey of Object.keys(uom)) {
+            if (uom[orbiteeKey].includes(orbiter)) {
+                path.unshift(orbiteeKey);
+                break;
+            }
+        }
+        if (path[0] !== 'COM') {
+            buildPath(path);
+        }
+        return path;
+    };
+
+    const findIntersection = (path1, path2) => {
+        if (path2.length > path1.length) {
+            [path1, path2] = [path2, path1];
+        }
+        return path1.filter(el => path2.includes(el)).pop();
+    };
+
+    const countTransfers = () => {
+        const youPath = buildPath(['YOU']);
+        const sanPath = buildPath(['SAN']);
+
+        // console.log(youPath);
+        // console.log(sanPath);
+
+        const intersection = findIntersection(youPath, sanPath);
+        // console.log(intersection);
+
+        return youPath.length - 2 - youPath.indexOf(intersection) + sanPath.length - 2 - sanPath.indexOf(intersection);
+    };
+
+    const transfers = countTransfers();
+    console.log(transfers);
 
     console.log('execution time', performance.now() - startTime);
 })();
