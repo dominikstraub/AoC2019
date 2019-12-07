07d8d71111a050b92c425fb78fc883aabdbad263
Dominik Straub <dominik.straub@gmail.com>
2019-12-07 11:46:17 +0100

    Day 7, Star 2

--- Day7/main.js
+++ Day7/main.js
@@ -1,8 +1,7 @@
 (async () => {
     const startTime = performance.now();
 
-    const intcode = (data, readInput) => {
-        let index = 0;
+    const intcode = (data, readInput, index = 0) => {
         let readInputIndex = 0;
 
         while (true) {
@@ -34,12 +33,15 @@
                     index += 4;
                     break;
                 case 3:
+                    if (readInputIndex >= readInput.length) {
+                        readInputIndex = readInput.length - 1;
+                    }
                     data[data[index + 1]] = readInput[readInputIndex++];
                     index += 2;
                     break;
                 case 4:
                     index += 2;
-                    return data[p1address];
+                    return [data[p1address], index];
                 case 5:
                     if (data[p1address] !== 0) {
                         index = data[p2address];
@@ -72,35 +74,56 @@
                     break;
                 case 99:
                     index += 1;
-                    return;
+                    return [];
             }
         }
     };
 
     const amplifierController = (software, phases) => {
         let input = 0;
-        for (let i = 0; i < 5; i++) {
-            input = intcode(software.slice(), [phases[i], input]);
+        const softwares = [software.slice(), software.slice(), software.slice(), software.slice(), software.slice()];
+        const indices = [0, 0, 0, 0, 0];
+        let init = true;
+        while (true) {
+            let output = input;
+            for (let i = 0; i < 5; i++) {
+                const input = [output];
+                if (init) {
+                    input.unshift(phases[i]);
+                }
+                [output, indices[i]] = intcode(softwares[i], input, indices[i]);
+                if (output === Number.POSITIVE_INFINITY) {
+                    throw new Error(output);
+                }
+                if (!output) {
+                    break;
+                }
+            }
+            init = false;
+            if (!output) {
+                break;
+            }
+            input = output;
         }
         return input;
     };
 
     const getMaxThrust = software => {
         let maxThrust = null;
-        for (let phase0 = 0; phase0 <= 4; phase0++) {
-            for (let phase1 = 0; phase1 <= 4; phase1++) {
+        for (let phase0 = 5; phase0 <= 9; phase0++) {
+            for (let phase1 = 5; phase1 <= 9; phase1++) {
                 if (phase0 === phase1) {
                     continue;
                 }
-                for (let phase2 = 0; phase2 <= 4; phase2++) {
+                for (let phase2 = 5; phase2 <= 9; phase2++) {
                     if (phase0 === phase2 || phase1 === phase2) {
                         continue;
                     }
-                    for (let phase3 = 0; phase3 <= 4; phase3++) {
+                    for (let phase3 = 5; phase3 <= 9; phase3++) {
                         if (phase0 === phase3 || phase1 === phase3 || phase2 === phase3) {
                             continue;
                         }
-                        for (let phase4 = 0; phase4 <= 4; phase4++) {
+                        for (let phase4 = 5; phase4 <= 9; phase4++) {
                             if (phase0 === phase4 || phase1 === phase4 || phase2 === phase4 || phase3 === phase4) {
                                 continue;
                             }
@@ -121,6 +144,8 @@
     // const response = await fetch('./test.json');
     // const response = await fetch('./test2.json');
     // const response = await fetch('./test3.json');
+    // const response = await fetch('./test4.json');
+    // const response = await fetch('./test5.json');
     const input = (await response.json()).data;
     // console.log(input);
 
