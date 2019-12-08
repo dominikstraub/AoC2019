f23a079b0c4f973a42b2e888356010e92739fa08
Dominik Straub <dominik.straub@gmail.com>
2019-12-08 10:28:09 +0100

    first attempt

--- Day8/main.js
+++ Day8/main.js
@@ -13,9 +13,9 @@
             for (let rowI = 0; rowI < rows; rowI++) {
                 layer[rowI] = [];
                 const row = layer[rowI];
-                for (let col = 0; col < cols; col++) {
-                    row[col] = Number.parseInt(data[i++], 10);
-                    digitCounts[row[col]]++;
+                for (let colI = 0; colI < cols; colI++) {
+                    row[colI] = Number.parseInt(data[i++], 10);
+                    digitCounts[row[colI]]++;
                 }
             }
             if (!fevestZeros || digitCounts[0] < fevestZeros) {
@@ -27,13 +27,42 @@
         return { image, checksum };
     };
 
+    const render = image => {
+        const renderedImage = [];
+
+        for (let colI = 0; colI < image[0][0].length; colI++) {
+            renderedImage[colI] = [];
+            for (let rowI = 0; rowI < image[0].length; rowI++) {
+                renderedImage[colI][rowI] = null;
+                for (let layerI = 0; layerI < image.length; layerI++) {
+                    if (renderedImage[colI][rowI] === null || renderedImage[colI][rowI] === 2) {
+                        renderedImage[colI][rowI] =
+                            image[layerI][rowI][colI] === 1
+                                ? '#'
+                                : image[layerI][rowI][colI] === 0
+                                ? ' '
+                                : image[layerI][rowI][colI];
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return renderedImage;
+    };
+
     const response = await fetch('./input.json');
     // const response = await fetch('./test.json');
+    // const response = await fetch('./test2.json');
     const { data, width, height } = await response.json();
     // console.log(input);
 
     const { image, checksum } = dsnDecoder(data, width, height);
     console.log(checksum, image);
+
+    const renderedImage = render(image);
+    console.log(renderedImage);
 
     console.log('execution time', performance.now() - startTime);
 })();
